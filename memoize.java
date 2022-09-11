f97f5833a128a780dde648e8fbbe0e6db6653641
Subratjain <103668727+Subratjain@users.noreply.github.com>
2022-09-11 09:06:35 +0530

    Create memoize.java

---
+++ memoize.java
@@ -0,0 +1,52 @@
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+*/
+
+class memoize {
+    private int[] dp = new int[31];
+    public int memoize(int n){
+            if(dp[n]!=0){
+                return dp[n];
+            }
+            if(n<3){
+                return 1;
+            }
+            else{
+                dp[n]=memoize(n-1)+memoize(n-2);
+                return memoize(n);
+            }
+        
+    }
+    public int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        if(n==2){
+            return 1;
+        }
+        memoize(n);
+        return dp[n];
+    }
+}
